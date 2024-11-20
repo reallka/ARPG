bb9ecced9863371c8b640ad0c797d3073c2f97bf
Edwin-Godot <19mukwayae@steds.org.uk>
2024-11-20 20:41:16 +0000

    Episode-17

--- GUI/pause_menu/pause_menu.gd
+++ GUI/pause_menu/pause_menu.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 signal shown
 signal hidden
 
+@onready var audio_stream_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var button_save: Button = $Control/HBoxContainer/Button_Save
 @onready var button_load: Button = $Control/HBoxContainer/Button_Load
 @onready var item_description: Label = $Control/ItemDescription
@@ -60,3 +61,8 @@ func _on_load_pressed() -> void:
 
 func update_item_description( new_text : String ) -> void:
 	item_description.text = new_text
+
+
+func play_audio( audio : AudioStream ) -> void:
+	audio_stream_player.stream = audio
+	audio_stream_player.play()
